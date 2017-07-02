0d0e1a7a2d25e14ba10fcbbf541cbdda31b587ed
chencong <1042738887@qq.com>
2017-07-02 23:05:53 +0800

    first commit,学习多线程

--- studyThread/src/cc/ccoder/studyThreadAPI/MyThread.java
+++ studyThread/src/cc/ccoder/studyThreadAPI/MyThread.java
@@ -9,6 +9,6 @@ package cc.ccoder.studyThreadAPI;
  */
 public class MyThread {
     public static void main(String[] args) {
-        System.out.println("nihao");
+        System.out.println("Hello Word");
     }
 }
