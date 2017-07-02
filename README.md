884539a8b812e59a85cdf0fa5085fa07429e9b21
chencong <1042738887@qq.com>
2017-07-02 22:55:21 +0800

    first commit,学习多线程

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JavaStudyBasic
+## 《Java多线程编程核心技术》学习笔记
+  >&nbsp;&nbsp;在学习了Mybatis和Hibernate这类ORM框架后，趁着暑假的时间，自己好好学习一下很早就准备学习的多线程。
+  顺便将自己学习这本书的过程和一系列思考记录下来。
---
+++ studyThread/src/cc/ccoder/studyThreadAPI/MyThread.java
@@ -0,0 +1,11 @@
+package cc.ccoder.studyThreadAPI;
+
+/**
+ * 项目：  JavaStudyBasic
+ * 包名：  cc.ccoder.studyThreadAPI
+ * 作者：  chencong
+ * 时间：  2017/7/2 22:46.
+ * 描述：  我的第一个线程类
+ */
+public class MyThread {
+}
